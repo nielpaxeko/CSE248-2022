aa74e4a9f38a210adacf6ff91533cc1a3eed572f
Edgar Pacheco <nielpaxeko@gmail.com>
2022-09-17 20:50:47 -0400

    Initial Commit

--- GIT/src/Push.java
+++ GIT/src/Push.java
@@ -4,6 +4,7 @@ public class push {
 	public static void main(String[] args) {
 		System.out.println("This is a test");
 		System.out.println("Hello World");
+		System.out.println("I love pizza");
 
 	}
 
