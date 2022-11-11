45db2cfb25262ae50382419fd37214aac8d76c15
Edgar Pacheco <90057170+nielpaxeko@users.noreply.github.com>
2022-11-11 16:10:55 -0500

    Newsletter Sign Up project
    This project is from Udemy course, works perfectly except I decided to not make it a live server on heroku

---
+++ NewsletterSignUp/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing"); // you need to add dependency first. See tips.
+ 
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+ 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+ 
+client.setConfig({
+    apiKey: "deada3b92af26f2c1609a88fafd89995-us9",
+    server: "us9"
+});
+ 
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+ 
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember("0335bdf605", {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+        }
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+ 
+  run();
+});
+ 
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+app.post("/success", function(req, res) {
+    res.redirect("/");
+  });
+ 
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000.");
+});
+
+
+
